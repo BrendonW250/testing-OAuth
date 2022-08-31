920c1df7c596545a78062ca6485ec4f2f77f5968
BrendonW250 <brendonwright16@gmail.com>
2022-08-30 23:51:40 -0400

    slight-change

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,9 @@ const router = express.Router()
 //@description    login/landing page
 //@route   GET/
 router.get('/', (req, res) => {
-    res.render('login')
+    res.render('login', {
+        layout: 'login'
+    })
 })
 
 
